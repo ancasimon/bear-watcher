f645085e0cda51a53ba1ff77dea000a4e1de8fe4
Anca Simon <ancarsimon@gmail.com>
2020-02-27 23:00:34 -0600

    added bears in the river

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 import bearForm from './components/bearForm.js';
 import river from './components/river.js';
+import bearData from './helpers/data/bearData.js';
 
 const init = () => {
     bearForm.buildBearForm();
+    river.buildBearSleuth();
 };
 
 init();
