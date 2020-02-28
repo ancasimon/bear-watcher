eb41048dd8994140433031d85082aae16b26d0f8
Anca Simon <ancarsimon@gmail.com>
2020-02-27 23:36:14 -0600

    added tried button

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -10,12 +10,25 @@ const buildBearSleuth = () => {
             domString += `<img src="${allBears[i].imageUrl}" class="card-img-top" alt="bear-photo">`;
             domString += '<div class="card-body">';
                 domString += `<h5 class="card-title">${allBears[i].name}</h5>`;
+                domString += `<button type="button" class="btn btn-outline-warning bear-tried-button">Tried to...</button>`;
             domString += '</div>';
         domString += '</div>';
     }
     domString += '</div>';
     util.printToDom('bear-container', domString);
-    console.log("adding a bear");
+    bearActivityFailureEvent();
 };
+
+const trackFailure = (e) => {
+    console.log('tried but failed miserably...');
+};
+
+const bearActivityFailureEvent = () => {
+    const triedButtons = document.getElementsByClassName('bear-tried-button');
+    for(let i = 0; i < triedButtons.length; i++){
+      triedButtons[i].addEventListener('click', trackFailure);
+    }
+  };
+
 
 export default { buildBearSleuth };
